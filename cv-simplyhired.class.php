5ad7a4cd8a4f7dc4a46915c989af676649c04260
Evan Donovan <evan@evandonovan.org>
2012-04-30 16:16:34 -0400

    Remove debugging

--- cv-simplyhired.class.php
+++ cv-simplyhired.class.php
@@ -273,10 +273,8 @@ class CV_SimplyHired_API extends SimplyHired_API {
 		  // Correct the country code for Great Britain.
 		  $gb_countries = array('ENGLAND', 'SCOTLAND', 'NORTHERN IRELAND'); // England, Scotland, Northern Ireland
 		  if(in_array($country_code, $gb_countries)) {
-		  	echo 'Scotland was matched.';
 		  	$country_code = 'GB';
 		  }
-		  echo 'Country code: ' . $country_code;
 		  $lJobsArray[$i]['country'] = $country_code;
 		  // Get the created and changed dates.
 		  $lJobsArray[$i]['created'] = strtotime(xt_getInnerXML($res->dp));
